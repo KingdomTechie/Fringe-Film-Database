3be42d72e18070e53d5d10585c82555303ed99a6
Keith Powell <keithpowell@Keiths-MacBook-Air.local>
2021-04-26 11:02:09 -0700

    Added initial boilerplate

---
+++ server.js
@@ -0,0 +1,46 @@
+
+//TODO - Have full CRUD functionality
+/* 
+  CRUD Functionality
+  C-reate
+  R-ead
+  U-pdate
+  D-estroy
+*/
+
+// External Modules
+const express = require("express");
+const methodOverride = require("method-override");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const bcrypt = require("bcryptjs");
+
+// Internal Modules
+
+
+// Instanced Modules
+const app = express();
+
+// Configuration Variables
+
+
+// App Configuration
+app.set("view engine", "ejs")
+
+// Middleware
+
+
+// Session middleware takes in a config object
+
+// Logger Middleware (helper tool) - make sure to pass in req object, resp object, and next
+
+// Controllers
+
+// Homepage
+
+// Server Bind
+
+
+
+
+
