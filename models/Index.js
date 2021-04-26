c42d244586ae97c5674eedfe859de1abdd13273c
Keith Powell <keithpowell@Keiths-MacBook-Air.local>
2021-04-26 15:05:02 -0700

    Adding mongoDB/Atlas to application, it's buggy

---
+++ models/Index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+require("dotenv").config()
+
+const mongoURL = process.env.MONGO_URI
+
+mongoose.connect(mongoURL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
+});
+
+mongoose.connection.on("connected", function () {
+	console.log("MongoDB connected! :)");
+});
+
+mongoose.connection.on("disconnected", function () {
+	console.log("MongoDB disconnected! :(");
+});
+
+mongoose.connection.on("err", function () {
+	console.log("MongoDB error! Oh no!", err);
+});
+
+module.exports = {
+    
+}
